721a32d9475827ae869c44a20d296688165fd704
Lee <richmond.liew.97@gmail.com>
2020-01-19 14:57:52 -0500

    sorted function

--- homework1.c
+++ homework1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 int* generateNumbers() {
     static int returnArr[1000000];
@@ -12,10 +13,71 @@ int* generateNumbers() {
     return returnArr; 
 }
 
+void merge(int arr[], int l, int m, int r) {
+    int leftPtr, rightPtr, mergePtr;
+    int sizeLeft = m - l + 1;
+    int sizeRight = r - m;
+
+    int left[sizeLeft], right[sizeRight];
+
+    for(int i = 0; i < sizeLeft; i++) {
+        left[i] = arr[l + i];
+    }
+    for(int j = 0; j < sizeRight; j++) {
+        right[j] = arr[m + 1 + j];
+    }
+
+    leftPtr = 0;
+    rightPtr = 0;
+    mergePtr = l;
+
+    while(leftPtr < sizeLeft && rightPtr < sizeRight) {
+        if(left[leftPtr] <= right[rightPtr]) {
+            arr[mergePtr] = left[leftPtr];
+            leftPtr++;
+        }
+        else {
+            arr[mergePtr] = right[rightPtr];
+            rightPtr++;
+        }
+        mergePtr++;
+    }
+
+
+    while(leftPtr < sizeLeft) {
+        arr[mergePtr] = left[leftPtr];
+        leftPtr++;
+        mergePtr++;
+    }
+
+    while(rightPtr < sizeRight) {
+        arr[mergePtr] = right[rightPtr];
+        rightPtr++;
+        mergePtr++;
+    }
+
+}
+
+void mergeSort(int arr[], int l, int r) {
+        if (l < r) 
+    { 
+        int m = l+(r-l)/2; 
+
+        mergeSort(arr, l, m); 
+        mergeSort(arr, m+1, r); 
+  
+        merge(arr, l, m, r); 
+    } 
+
+}
+
 int main() {
     int* randNumbers = generateNumbers();
-    for(int i = 0; i < 20; i++){
-        printf("%d \n", randNumbers[i]);
+
+    mergeSort(randNumbers, 0, 99999);
+
+    for(int i = 0; i < 50; i++){
+        printf("%i \n", randNumbers[i]);
     }
     return 0;
 }
