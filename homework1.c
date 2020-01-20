cda1425093a045ebd1b8b5d3b8a260d1dbc21100
Lee <richmond.liew.97@gmail.com>
2020-01-20 18:41:24 -0500

    gprof makefile

--- homework1.c
+++ homework1.c
@@ -86,6 +86,10 @@ void mergeSort(int arr[], int l, int r, bool ascending) {
 }
 
 int main() {
+
+    clock_t t;
+    t = clock();
+
     int* randNumbers = generateNumbers();
     int secondHalf[500000];
 
@@ -96,9 +100,13 @@ int main() {
     mergeSort(randNumbers, 0, 999999, true);
     mergeSort(secondHalf, 0, 499999, false);
 
-    for(int i = 0; i < 50; i++){
-        printf("%i \n", randNumbers[i]);
-        printf("%i \n", secondHalf[i]);
-    }
+    // for(int i = 0; i < 50; i++){
+    //     printf("%i \n", randNumbers[i]);
+    //     printf("%i \n", secondHalf[i]);
+    // }
+
+    t = clock() - t;
+    double exec_time = ((double) t)/ CLOCKS_PER_SEC;
+    printf("Execution Time: %f\n", exec_time);
     return 0;
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 all: homework1.c
-	gcc -std=c99 -o test homework1.c
+	gcc -std=c99 -g -pg -o test homework1.c
 
 clean:
 	$(RM) test
